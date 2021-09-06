34b98e8f54aad50e4b6043aaf1118baca622ea90
Serhii Krimets <75704401+Sergio1004@users.noreply.github.com>
2021-09-06 10:52:12 +0300

    add local storage

--- src/App.js
+++ src/App.js
@@ -10,14 +10,22 @@ import { v1 as uuid } from "uuid";
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
+    contacts: [],
     filter: "",
   };
+
+  componentDidMount() {
+    const contactsStorage = JSON.parse(localStorage.getItem("contacts"));
+    if (contactsStorage) {
+      this.setState({ contacts: contactsStorage });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
 
   addContact = ({ name, number }) => {
     if (
